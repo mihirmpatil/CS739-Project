1eeeefa0f52f5a1e63435d5cd8bc63a6c83c20d2
Mihir Patil <mihirmpatil@gmail.com>
2015-11-26 15:26:06 -0600

    sequential read and write done

---
+++ disk/seq_read_test.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define BUFFER_SIZE 4096
+#define BILLION 1000000000L
+
+uint64_t time_diff(struct timespec start, struct timespec end)
+{
+		return BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+}
+
+int main(int argc, char *argv[])
+{
+	if(argc != 2)
+	{
+		printf("Usage: binary filename\n");
+	}
+	uint64_t diff;
+	struct timespec start, end;
+	char filename[1024];
+	strcpy(filename, argv[1]);
+
+	int fd = open(filename, O_RDONLY);
+	char buffer[BUFFER_SIZE];
+	int total_read=0, num_read=0, times=0;
+
+	int i, iterations = 10;
+
+	for(i=0; i<iterations; i++)
+	{
+		total_read = 0;
+		num_read = 0;
+		lseek(fd, 0, SEEK_SET);
+		clock_gettime(CLOCK_REALTIME, &start);
+		while((num_read = read(fd, buffer, BUFFER_SIZE)) > 0)
+		{
+			total_read += num_read;
+			times++;
+		}
+		clock_gettime(CLOCK_REALTIME, &end);
+		diff = time_diff(start, end);
+		printf("Time for read of %d bytes from %s - %llu\n", total_read, filename, (long long unsigned int)diff);
+		//system("echo 3 | sudo tee /proc/sys/vm/drop_caches");
+		system("sudo sh -c \"sync; echo 3 > /proc/sys/vm/drop_caches\"");
+	}
+	return 0;
+}
---
+++ disk/seq_write_test.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdint.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define BUFFER_SIZE 4096
+#define BILLION 1000000000L
+
+uint64_t time_diff(struct timespec start, struct timespec end)
+{
+		return BILLION * (end.tv_sec - start.tv_sec) + end.tv_nsec - start.tv_nsec;
+}
+
+int main(int argc, char *argv[])
+{
+	if(argc != 2)
+	{
+		printf("Usage: binary filename\n");
+	}
+	uint64_t diff;
+	struct timespec start, end;
+	char filename[1024];
+	strcpy(filename, argv[1]);
+
+	char buffer[BUFFER_SIZE];
+	int i;
+	for(i=0; i<BUFFER_SIZE-1; i++)
+		buffer[i] = 'a';
+	buffer[BUFFER_SIZE] = '\0';
+	int fd, total_written=0, num_written=0, times=0;
+ 	int file_size=100*1024;											// This will be multiplied by BUFFER_SIZE
+	int k, iterations = 10;
+
+	for(k=0; k<iterations; k++)
+	{
+		total_written = 0;
+		num_written = 0;
+		fd = open(filename, O_CREAT | O_TRUNC | O_RDWR, 0640);
+		if(fd == -1)
+			perror("fd is -1");
+		lseek(fd, 0, SEEK_SET);
+		clock_gettime(CLOCK_REALTIME, &start);
+		for(i=0; i<file_size; i++)
+		{
+			num_written = write(fd, buffer, BUFFER_SIZE);
+			// fsync(fd);
+			if (num_written != BUFFER_SIZE)
+			{
+				printf("Error=> bytes written %d, BUFFER_SIZE %d\n", num_written, BUFFER_SIZE);
+				close(fd);
+				exit(1);
+			}
+			total_written += num_written;
+		}
+		fsync(fd);
+		clock_gettime(CLOCK_REALTIME, &end);
+		diff = time_diff(start, end);
+		printf("Time for write of %s, %d bytes - %llu\n", filename, total_written, (long long unsigned int)diff);
+		close(fd);
+	}
+	return 0;
+}
