89038bf692d6b13caf6d4b6f95a0224416092b18
Mihir Patil <mihirmpatil@gmail.com>
2015-11-29 21:36:41 -0600

    added memory pinning program

--- disk/seq_read_test.c
+++ disk/seq_read_test.c
@@ -18,6 +18,7 @@ int main(int argc, char *argv[])
 	if(argc != 2)
 	{
 		printf("Usage: binary filename\n");
+		exit(1);
 	}
 	uint64_t diff;
 	struct timespec start, end;
---
+++ memory/mem_stress.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <sys/mman.h>
+#include <string.h>
+
+int main(int argc, char *argv[])
+{
+	long long unsigned int to_allocate = (long long unsigned int)102*1024*1024*1024;
+	char *ptr = (char *)malloc(to_allocate*sizeof(char));
+	if (ptr == NULL)
+	{
+		perror("ptr is NULL\n");
+		exit(1);
+	}
+	memset(ptr, 0, to_allocate);
+	mlock(ptr, to_allocate);
+	printf("LOCKED!\n");
+	char a;
+	long long unsigned int x=0;
+	while(1)
+	{
+		*(ptr + x) = 'a';
+		x = ++x % to_allocate;
+		//printf("x=%llu\n",x);
+	}
+
+	return 0;
+}
