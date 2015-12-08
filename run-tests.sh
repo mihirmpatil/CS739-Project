97f722752242a76cc1232681ccadd29566ed6f0d
Mihir Patil <mihirmpatil@gmail.com>
2015-12-08 12:21:03 -0600

    modified run-tests script

--- run-tests.sh
+++ run-tests.sh
@@ -14,12 +14,13 @@ unzip master.zip
 
 
 echo "Start: " $(date +%s)
-cleanup
-setup
-cd CS739-Project-master
+#cleanup
+#setup
+#cd CS739-Project-master
 cd disk
 make
 
 ./seq_write file.1GB 262144 > seq_write_1GB.out
+sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
 ./seq_read file.1GB > seq_read_1GB.out
 echo "End: " $(date +%s)
