cd5c100dbc01cc5855606dc6e16b09f6b63e3a46
Mihir Patil <mihirmpatil@gmail.com>
2015-12-09 18:59:16 -0600

    updated runtest for memory

--- run-tests.sh
+++ run-tests.sh
@@ -13,14 +13,26 @@ unzip master.zip
 }
 
 
-echo "Start: " $(date +%s)
 #cleanup
 #setup
 #cd CS739-Project-master
+
+disk() {
 cd disk
 make
 
 ./seq_write file.1GB 262144 > seq_write_1GB.out
 sudo sh -c "sync; echo 3 > /proc/sys/vm/drop_caches"
 ./seq_read file.1GB > seq_read_1GB.out
+}
+
+
+memory() {
+cd memory
+gcc contmemtest.c -pthread -o contmemtest
+./contmemtest 1 0 4096 64 1 1 > contmemtest.out
+}
+
+echo "Start: " $(date +%s)
+memory
 echo "End: " $(date +%s)
